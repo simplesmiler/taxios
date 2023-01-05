2c20e44b4800ea552eb9f7cd0aae120b83e983f4
Denis Karabaza <denis.karabaza@gmail.com>
2023-01-05 13:26:36 +0300

    [chore] Fixed test cases

--- packages/taxios-sandbox/src/generated/IgnoreMinMaxItems.ts
+++ packages/taxios-sandbox/src/generated/IgnoreMinMaxItems.ts
@@ -1,23 +1,14 @@
 export namespace MinMaxItems {
   /**
    * List of tags, not to many
-   *
-   * @minItems 0
-   * @maxItems 3
    */
   export type Tags = string[];
   /**
    * Sequence of bytes, can be empty
-   *
-   * @minItems 0
-   * @maxItems 2147483647
    */
   export type Bytes = number[];
   /**
    * List of emails, at least one
-   *
-   * @minItems 1
-   * @maxItems 2147483647
    */
   export type Emails = string[];
 }
