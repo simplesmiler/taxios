0e4bdbf3c4f0e8409485ddad5e805c6dcd199b6d
Denis Karabaza <denis.karabaza@gmail.com>
2023-01-05 15:53:53 +0300

    [chore] Add test cases for nullable field support

---
+++ packages/taxios-sandbox/src/generated/Nullable30.ts
@@ -0,0 +1,15 @@
+export namespace Nullable {
+  export interface User {
+    name: string;
+    alias: string;
+  }
+}
+
+export interface Nullable {
+  version: '1';
+  routes: {
+    '/v1/test': {
+      GET: {};
+    };
+  };
+}
